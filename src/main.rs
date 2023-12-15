25bb7adddd016948608cda43edbb87816ef71951
MikeTeddyOmondi <mike_omondi@outlook.com>
2023-12-15 18:01:59 +0300

    Added new routes and handlers

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
-use axum::routing::{get, post};
-use axum::{extract::Json, response::Json as JsonResponse, Router};
+// use axum::extract::Query;
+use axum::routing::get;
+use axum::{
+    extract::{Json, Path},
+    response::Json as JsonResponse,
+    Router,
+};
+// use diesel::sql_query;
 use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
-use serde_json::{json, Value};
+use serde_json::{json, to_string, Value};
 use std::net::SocketAddr;
 // use tower_http::cors::CorsLayer;
 use tracing::{error, info};
@@ -28,8 +34,9 @@ async fn main() {
 
     // Building our application with a single Route
     let app = Router::new()
-        .route("/", get(get_todos))
-        .route("/", post(create_todos));
+        .route("/", get(get_api_info))
+        .route("/todos", get(get_todos).post(create_todos))
+        .route("/todos/:public_id", get(get_todo));
 
     // Run the server with hyper on http://127.0.0.1:5050
     let addr = SocketAddr::from(([127, 0, 0, 1], 5050));
@@ -41,6 +48,15 @@ async fn main() {
         .expect("API Server failed to initialize!");
 }
 
+async fn get_api_info() -> JsonResponse<Value> {
+    info!("[!] GET Request: API Info");
+
+    Json(json!({
+        "apiVersion": "v1.0",
+        "description": "Todos RESTful API built on Axum",
+    }))
+}
+
 async fn get_todos() -> JsonResponse<Value> {
     info!("[!] GET Request");
     let mut connection = establish_connection();
@@ -49,10 +65,46 @@ async fn get_todos() -> JsonResponse<Value> {
         .load::<Todo>(&mut connection)
         .expect("Error occurred while reading database!");
     // let data: Result<Vec<Todo>> = sql_query("SELECT * FROM todos ORDER BY id").load(&mut connection);
-    info!("[!] Read successful!");
+
+    let serialized_data = to_string(&data).unwrap();
+
+    info!("[!] Read all successful!");
+    info!("[!] Data: {:?}", serialized_data);
 
     Json(json!({
         "success": "true",
+        "data": data,
+    }))
+}
+
+async fn get_todo(Path(public_id): Path<Uuid>) -> JsonResponse<Value> {
+    info!("[!] GET Request: {}", public_id);
+    let mut connection = establish_connection();
+
+    let data: Vec<Todo> = todos
+        .filter(schema::todos::public_id.eq(public_id.to_string()))
+        .load::<Todo>(&mut connection)
+        .expect("Error occurred while reading database!");
+
+    if data.len() == 0 {
+        let err = Json(json!({
+            "success": "false",
+            "data": { "id": public_id.to_string(), "message": "Not found" }
+        }));
+        error!("[#] Error: {public_id} not found!");
+        return err;
+    }
+
+    let serialized_data = to_string(&data).unwrap();
+
+    info!("[!] Read: {} successful!", public_id);
+    info!("[!] Data: {:?}", serialized_data);
+
+    Json(json!({
+        "success": "true",
+        "params": {
+            "id": public_id.to_string()
+        },
         "data": data,
     }))
 }
@@ -84,7 +136,7 @@ async fn create_todos(Json(req_body): Json<UserInput>) -> JsonResponse<Value> {
         .execute(&mut connection)
         .expect("Error occurred while inserting todo");
 
-    info!("[!] Insert successful!");
+    info!("[!] Create: {} successful!", new_todo.public_id);
 
     Json(json!({
         "success": "true",
