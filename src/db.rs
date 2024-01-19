0339712d0211ed9f192ba2a6edb7d1f9288134a9
MikeTeddyOmondi <mike_omondi@outlook.com>
2024-01-19 17:03:16 +0300

    Updated database connection to Postgres

--- src/db.rs
+++ src/db.rs
@@ -2,11 +2,11 @@ use diesel::prelude::*;
 use dotenvy::dotenv;
 use std::env;
 
-pub fn establish_connection() -> SqliteConnection {
+pub fn establish_connection() -> PgConnection {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL")
         .expect("DATABASE_URL must be set");
-    SqliteConnection::establish(&database_url)
+    PgConnection::establish(&database_url)
         .expect(&format!("Error connecting to {}", database_url))
 }
