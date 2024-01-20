825abb72d2b3ffe2475cf536460df8ef6f9f9220
MikeTeddyOmondi <mike_omondi@outlook.com>
2024-01-20 12:09:54 +0300

    Auto generated migration files by diesel cli

--- migrations/2023-11-20-112702_todos/up.sql
+++ migrations/2023-11-20-112702_todos/up.sql
@@ -1,8 +1,8 @@
 -- Your SQL goes here
 CREATE TABLE todos (
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    id SERIAL PRIMARY KEY,
     public_id TEXT NOT NULL,
     title TEXT NOT NULL,
     description TEXT NOT NULL,
-    completed INTEGER NOT NULL DEFAULT FALSE
+    completed BOOLEAN NOT NULL DEFAULT FALSE
 );
